d9e2678efa16a140ba734167b066772ab7ce4803
bhaveshd17 <bhaveshdhake8@gmail.com>
2022-01-17 14:46:22 +0530

    event scroll

--- js/event_section.js
+++ js/event_section.js
@@ -1,7 +1,32 @@
+let time;
 function openCard(elem) {
-    let x = document.getElementsByClassName("s2-card-div");
-    for (let i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    document.getElementById(elem).style.display = "block";
+  let x = document.getElementsByClassName("s2-card-div");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(elem).style.display = "block";
+  clearTimeout(time)
+  divScroll(elem)
 }
+
+function divScroll(elem) {
+  let isPaused = false;
+  let x = document.getElementById(elem);
+  function scrollLogic() {
+    if (isPaused === false) {
+      x.scrollBy(0, 1);
+      time = setTimeout(scrollLogic, 20);
+    }
+  }
+  x.addEventListener("mouseenter", () => {
+    isPaused = true;
+    clearTimeout(time)
+  });
+  x.addEventListener("mouseleave", () => {
+    isPaused = false;
+    clearTimeout(time)
+    scrollLogic();
+  });
+  scrollLogic();
+}
+divScroll('news-and-event')
